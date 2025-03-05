454aa480e8abc6a501924ce8b3169a9d176b4844
Raimon <raimon.txollo@gmail.com>
2025-03-05 19:51:49 +0100

    Show word counter in index.html

--- src/ui/analysis.ts
+++ src/ui/analysis.ts
@@ -1,4 +1,4 @@
-import { getParagraphsTotal } from "../analytics/index.js";
+import { getParagraphsTotal, getWordsTotal } from "../analytics/index.js";
 
 const totalsContainer = document.querySelector(".totals");
 const shortWordsContainer = document.querySelector(".short");
@@ -123,4 +123,5 @@ export const analyzeText = (text: string): void => {
   listenForbiddenWordsChanges(text);
 
   renderParagraphsTotal(getParagraphsTotal(text));
+  renderWordsTotal(getWordsTotal(text));
 };
